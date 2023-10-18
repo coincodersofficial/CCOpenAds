9b682fe3185d6b6aa20121713e469849cb101969
Coin Coders <147849523+coincodersofficial@users.noreply.github.com>
2023-10-18 17:56:35 +0530

    Update MyApplication.java

--- app/src/main/java/com/coincoders/ccopenads/MyApplication.java
+++ app/src/main/java/com/coincoders/ccopenads/MyApplication.java
@@ -5,11 +5,7 @@ import android.app.Application;
 import com.google.android.gms.ads.MobileAds;
 
 public class MyApplication extends Application {
-//  git config --global user.name "Coin Coders"
-    // git config --global user.email "coincodersofficial@gmail.com"
-    //  git config user.name "Coin Coders"
-//git config user.email "coincodersofficial@gmail.com"
-    @Override    // Coin Coders   coincodersofficial@gmail.com
+    @Override   
     public void onCreate() {
         super.onCreate();
         MobileAds.initialize(this);
